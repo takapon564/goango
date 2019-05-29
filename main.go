446ff396e3b6e0e284d76a84a48132eccd71597a
matsuura takahito <ku.takahito058@gmail.com>
2019-05-29 11:03:31 +0900

    空文字を入力した場合再入力させる

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"encoding/base64"
 	"fmt"
+	"os"
 )
 
 func main() {
-	var plainText string
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("base64 encoder/decoder tool")
 	fmt.Print("enter plain text:")
-	fmt.Scan(&plainText)
+	var plainText string
+	for scanner.Scan() {
+		plainText = scanner.Text()
+		if len(plainText) == 0 {
+			fmt.Println("Empty character is not allowed. Enter character.")
+			fmt.Print("enter plain text:")
+			continue
+		} else {
+			break
+		}
+	}
 	data := []byte(plainText)
 	encodeText := base64.StdEncoding.EncodeToString(data)
 	fmt.Println("result: ", encodeText)
