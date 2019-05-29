e8a64f0cc0126a489de35d33fe18de6846dd59fe
matsuura takahito <ku.takahito058@gmail.com>
2019-05-29 11:25:31 +0900

    add decode function

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 )
 
 func main() {
+	var plainText string
+	var decodeStringAsPlain string
 	scanner := bufio.NewScanner(os.Stdin)
+	// encoder
 	fmt.Println("base64 encoder/decoder tool")
 	fmt.Print("enter plain text:")
-	var plainText string
 	for scanner.Scan() {
 		plainText = scanner.Text()
 		if len(plainText) == 0 {
@@ -22,7 +24,26 @@ func main() {
 			break
 		}
 	}
-	data := []byte(plainText)
-	encodeText := base64.StdEncoding.EncodeToString(data)
-	fmt.Println("result: ", encodeText)
+	dataForEncode := []byte(plainText)
+	encodeString := base64.StdEncoding.EncodeToString(dataForEncode)
+	fmt.Println("result: ", encodeString)
+
+	// decoder
+	fmt.Print("enter decode string: ")
+	for scanner.Scan() {
+		decodeStringAsPlain = scanner.Text()
+		if len(decodeStringAsPlain) == 0 {
+			fmt.Println("Empty character is not allowed. Enter character.")
+			fmt.Print("enter decode string: ")
+			continue
+		} else {
+			break
+		}
+	}
+	decodeStringAsByte, err := base64.StdEncoding.DecodeString(decodeStringAsPlain)
+	if err != nil {
+		fmt.Println("error", err)
+		return
+	}
+	fmt.Println("reslut: ", string(decodeStringAsByte))
 }
